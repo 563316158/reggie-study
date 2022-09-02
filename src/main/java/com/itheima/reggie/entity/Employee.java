b056af22f457127aba6ed58f50acc83e3a2ca779
Jerry.Qin 秦利杰 <jerry.qin@youniverse.cc>
2022-09-02 09:32:02 +0800

    first commit

---
+++ src/main/java/com/itheima/reggie/entity/Employee.java
@@ -0,0 +1,48 @@
+package com.itheima.reggie.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 员工实体
+ */
+@Data
+public class Employee implements Serializable {
+    /**
+     *     serializable接口的作用：1、存储对象在存储介质中，以便在下次使用的时候，可以很快捷的重建一个副本；2、便于数据传输，尤其是在远程调用的时候。
+     */
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String username;
+
+    private String name;
+
+    private String password;
+
+    private String phone;
+
+    private String sex;
+
+    private String idNumber;//身份证号码
+
+    private Integer status;
+
+    @TableField(fill = FieldFill.INSERT) //插入时填充字段
+    private LocalDateTime createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE) //插入和更新时填充字段
+    private LocalDateTime updateTime;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+}
